cef4cefaff22fb98fe982ba1a7e8eb0cb7a7d428
Sufyan <msufyanfgeis@gmail.com>
2024-12-24 15:33:45 +0500

    initail commint

---
+++ javascript.js
@@ -0,0 +1,12 @@
+let x=76;
+
+const num = document.getElementById("num1").innerHTML;
+const message = document.getElementById("mess").innerHTML;
+
+numElement.textcontent= x;
+
+if (x>=75){
+    messageElement.textContent='Good';
+} else {
+    messageElement.textContent='Bad';
+}
